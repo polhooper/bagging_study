10c960dee93b66014753a0d9226a434d9e711f23
Aaron Polhamus <aaronpolhamus@gmail.com>
2015-12-09 12:49:45 -0800

    update to gains chart to make it cumulative gains. create modular functions for ggplot graphs in visualization script.

--- visuals.R
+++ visuals.R
@@ -4,16 +4,27 @@ sapply(pkg_list, require, character.only = TRUE)
 
 source('utils/r_utils.R')
 
+#..creat graph directory if it doesn't exist
+#-------------------------------------------
 graph_dir <- file.path(getwd(), 'graphics')
 if(!exists(graph_dir)) dir.create(graph_dir)
 
 PlotTabber <- function(indata, nbins, truths){ 
   avgs <- lapply(indata, function(x) tapply(truths, scorefunction(-x, nbins = nbins), mean))
-  avgs <- lapply(avgs, function(x) round(cumsum(x)/(1:length(x)), 4))
+  #avgs <- lapply(avgs, function(x) round(cumsum(x)/(1:length(x)), 4))
   plot_tab <- do.call('cbind.data.frame', avgs)
-  do.call('rbind.data.frame', lapply(names(plot_tab), function(x) data.frame(model = x, rate = plot_tab[[x]], bin = 1:nrow(plot_tab))))
+  do.call('rbind.data.frame', lapply(names(plot_tab), function(x){
+    rate <- plot_tab[[x]]
+    data.frame(
+      model = x, 
+      rate = rate,
+      cumrate = cumsum(rate)/(1:length(rate)),
+      bin = 1:nrow(plot_tab))
+  }))
 }
 
+#..define a couple helper functions
+#----------------------------------
 GainCast <- function(indata){
   #..this works on a data object that has first been processed with PlotTabber 
   ddply(indata, .(model), function(SUBSET){
@@ -29,6 +40,34 @@ GainCast <- function(indata){
   })
 }
 
+GainPlot <- function(indata, title, nbins, colors){ 
+  p <- ggplot(indata, aes(x = bin, y = rate, color = model)) + 
+    geom_point() + 
+    geom_line(size = 2) + 
+    geom_abline(intercept = 0, slope = 1/10, color = 'grey60', size = 1, linetype = 2) + 
+    scale_x_continuous(breaks = 0:nbins, labels = paste0(10*(0:nbins), '%'), name = '% Customers Contacted') + 
+    scale_y_continuous(breaks = seq(0, 1, 0.1), labels = percent, name = '% Positive Responses') + 
+    scale_color_manual(values = colors, name = '') + 
+    theme(text = element_text(size = 15)) +
+    ggtitle(title)
+  return(p)
+}
+
+LiftPlot <- function(indata, title, nbins, colors, truths){
+  p <- ggplot(indata, aes(x = bin, y = cumrate, color = model)) + 
+    geom_point() + 
+    geom_line(size = 2) + 
+    geom_abline(intercept = mean(truths), slope = 0, color = 'grey60', size = 1, linetype = 2) + 
+    scale_x_continuous(breaks = 0:nbins, labels = paste0(10*(0:nbins), '%'), name = '% Customers Contacted') + 
+    scale_y_continuous(breaks = seq(min(indata$cumrate), max(indata$cumrate), length.out = 10), labels = percent, name = 'Observed response rate (%)') + 
+    scale_color_manual(values = colors, name = '') + 
+    theme(text = element_text(size = 15)) +
+    ggtitle(title)
+  return(p)
+}
+
+#..import predictions and truths 
+#-------------------------------
 file_list <- dir()
 file_list <- file_list[grepl('_preds', file_list)]
 pred_frame <- do.call('cbind.data.frame', lapply(file_list, function(x) read.table(x, header = FALSE)[, 1]))
@@ -41,16 +80,13 @@ tmp <- select(pred_frame, lm, lm_bagged)
 plot_tab <- PlotTabber(tmp, 10, truths)
 color_vals <- c(brewer.pal(length(unique(plot_tab$model)), "Accent"))[-3]
 
-nbins <- 10 
-p1 <- ggplot(plot_tab, aes(x = bin, y = rate, color = model)) + 
-  geom_point() + 
-  geom_line(size = 2) + 
-  geom_abline(intercept = mean(truths), slope = 0, color = 'grey60', size = 1, linetype = 2) + 
-  scale_x_continuous(breaks = 1:nbins, labels = paste0(10*(1:nbins), '%'), name = '% Customers Contacted') + 
-  scale_y_continuous(breaks = seq(min(plot_tab$rate), max(plot_tab$rate), length.out = 10), labels = percent, name = 'Observed response rate (%)') + 
-  scale_color_manual(values = color_vals, name = '') + 
-  theme(text = element_text(size = 15)) +
-  ggtitle('Comparison of lift curves for a simple linear regression\nversus an ensemble of patched linear regressions')
+p1 <- LiftPlot(
+  indata = plot_tab, 
+  title = 'Comparison of lift curves for a simple linear regression\nversus an ensemble of patched linear regressions', 
+  nbins = 10, 
+  colors = color_vals, 
+  truths = truths)
+
 png(file.path(graph_dir, 'lift_ols.png'), width = 700, height = 400)
   print(p1)
 dev.off()
@@ -58,16 +94,12 @@ dev.off()
 #..cumulative gains chart, OLS models
 #------------------------------------
 gain_tab <- GainCast(plot_tab)
-nbins <- 10 
-p2 <- ggplot(gain_tab, aes(x = bin, y = rate, color = model)) + 
-  geom_point() + 
-  geom_line(size = 2) + 
-  geom_abline(intercept = 0, slope = 1/10, color = 'grey60', size = 1, linetype = 2) + 
-  scale_x_continuous(breaks = 1:nbins, labels = paste0(10*(1:nbins), '%'), name = '% Customers Contacted') + 
-  scale_y_continuous(breaks = seq(0, 1, 0.1), labels = percent, name = '% Positive Responses') + 
-  scale_color_manual(values = color_vals, name = '') + 
-  theme(text = element_text(size = 15)) +
-  ggtitle('Comparison of cumulative gains curves for a simple linear regression\n versus an ensemble of patched linear regressions')
+p2 <- GainPlot(
+  indata = gain_tab, 
+  title = 'Comparison of cumulative gains curves for a simple linear regression\n versus an ensemble of patched linear regressions', 
+  nbins = 10, 
+  colors = color_vals)
+
 png(file.path(graph_dir, 'gain_ols.png'), width = 700, height = 600)
   print(p2)
 dev.off()
@@ -76,18 +108,15 @@ dev.off()
 #------------------------
 tmp <- select(pred_frame, lm, lm_bagged, log, rf)
 plot_tab <- PlotTabber(tmp, 10, truths)
-
 color_vals <- c(brewer.pal(length(unique(plot_tab$model)), "Accent"))
-nbins <- 10 
-p3 <- ggplot(plot_tab, aes(x = bin, y = rate, color = model)) + 
-  geom_point() + 
-  geom_line(size = 2) + 
-  geom_abline(intercept = mean(truths), slope = 0, color = 'grey60', size = 1, linetype = 2) + 
-  scale_x_continuous(breaks = 1:nbins, labels = paste0(10*(1:nbins), '%'), name = '% Customers Contacted') + 
-  scale_y_continuous(breaks = seq(min(plot_tab$rate), max(plot_tab$rate), length.out = 10), labels = percent, name = 'Observed response rate (%)') + 
-  scale_color_manual(values = color_vals, name = '') + 
-  theme(text = element_text(size = 15)) +
-  ggtitle('Comparison of lift curves for random forest, linear model,\n logistic model and bagged linear model')
+
+p3 <- LiftPlot(
+  indata = plot_tab, 
+  title = 'Comparison of lift curves for random forest, linear model,\n logistic model and bagged linear model', 
+  nbins = 10, 
+  colors = color_vals, 
+  truths = truths)
+
 png(file.path(graph_dir, 'lift_all.png'), width = 700, height = 400)
   print(p3)
 dev.off()
@@ -96,16 +125,13 @@ dev.off()
 #------------------------------------
 gain_tab <- GainCast(plot_tab)
 color_vals <- c(brewer.pal(length(unique(plot_tab$model)), "Accent"))
-nbins <- 10 
-p4 <- ggplot(gain_tab, aes(x = bin, y = rate, color = model)) + 
-  geom_point() + 
-  geom_line(size = 2) + 
-  geom_abline(intercept = 0, slope = 1/10, color = 'grey60', size = 1, linetype = 2) + 
-  scale_x_continuous(breaks = 1:nbins, labels = paste0(10*(1:nbins), '%'), name = '% Customers Contacted') + 
-  scale_y_continuous(breaks = seq(0, 1, 0.1), labels = percent, name = '% Positive Responses') + 
-  scale_color_manual(values = color_vals, name = '') + 
-  theme(text = element_text(size = 15)) +
-  ggtitle('Cumulative gains curves for random forest, linear model,\n logistic model and bagged linear model')
+
+p4 <- GainPlot(
+  indata = gain_tab, 
+  title = 'Cumulative gains curves for random forest, linear model,\n logistic model and bagged linear model', 
+  nbins = 10, 
+  colors = color_vals)
+
 png(file.path(graph_dir, 'gain_all.png'), width = 700, height = 600)
   print(p4)
 dev.off()
